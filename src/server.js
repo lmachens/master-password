4a3ebf1a916c62cad002bd782fe8a1600adf44d1
lmachens <leon.machens@googlemail.com>
2019-10-02 15:15:09 +0200

    post secret to server

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ const http = require("http");
 const url = require("url");
 const fs = require("fs");
 
-const { get } = require("./lib/commands");
+const { get, set } = require("./lib/commands");
 
 const server = http.createServer(function(request, response) {
   const { pathname } = url.parse(request.url);
+  console.log(request.url, request.method);
 
   if (pathname === "/favicon.ico") {
     response.writeHead(404);
@@ -17,17 +18,26 @@ const server = http.createServer(function(request, response) {
     return response.end(content);
   }
 
-  console.log(pathname);
   try {
     const path = pathname.slice(1);
-    const secret = get("asd", path);
-
-    response.write(secret);
+    if (request.method === "GET") {
+      const secret = get("asd", path);
+      response.end(secret);
+    } else if (request.method === "POST") {
+      let body = "";
+      request.on("data", function(data) {
+        body += data;
+        console.log("Partial body: " + body);
+      });
+      request.on("end", function() {
+        console.log("Body: " + body);
+        set("asd", path, body);
+        response.end(`Set ${path}`);
+      });
+    }
   } catch (error) {
-    response.write("Can not read secret");
+    response.end("Can not read secret");
   }
-
-  response.end();
 });
 
 server.listen(8080, () => {
